2214406a3265b087aa3a6f93ef866afcc8c310b6
dev <pokhriya.dev@gmail.com>
2022-08-15 19:01:11 +0530

    lkj

--- aws-config/main.tf
+++ aws-config/main.tf
@@ -77,9 +77,40 @@ resource "aws_organizations_organization" "example" {
   feature_set                   = "ALL"
 }
 
-resource "aws_config_organization_managed_rule" "example" {
+resource "aws_config_organization_managed_rule" "s3-public-compliance-rule" {
   depends_on = [aws_organizations_organization.example]
 
-  name            = "example"
+  name            = "s3-public-compliance-rule"
   rule_identifier = "S3_BUCKET_LEVEL_PUBLIC_ACCESS_PROHIBITED"
 }
+
+############### Volume encryption EC2 ################
+
+resource "aws_config_organization_managed_rule" "ec2-volume-encryption-rule" {
+  depends_on = [aws_organizations_organization.example]
+
+  name            = "ec2-volume-encryption-rule"
+  rule_identifier = "ENCRYPTED_VOLUMES"
+}
+
+
+############### restrict  Incoming SSH  ################
+
+resource "aws_config_organization_managed_rule" "restrict-incoming-ssh-rule" {
+  depends_on = [aws_organizations_organization.example]
+
+  name            = "restrict-incoming-ssh-rule"
+  rule_identifier = "INCOMING_SSH_DISABLED"
+}
+
+
+
+############### restrict  default traffic to VPC   ################
+
+resource "aws_config_organization_managed_rule" "restrict-default-traffic-vpc-rule" {
+  depends_on = [aws_organizations_organization.example]
+
+  name            = "restrict-default-traffic-vpc-rule"
+  rule_identifier = "VPC_DEFAULT_SECURITY_GROUP_CLOSED"
+}
+
