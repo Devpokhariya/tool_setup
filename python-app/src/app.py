d24a3e0a260a8e06a52b75b0956fd2f8b7ffe970
dev <pokhriya.dev@gmail.com>
2022-09-12 22:15:15 +0530

    pyhton ui

---
+++ python-app/src/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Move to Microsoft Teams")
+st.header("Enter your Details -")
+phone = st.text_input("Mobile Number")
+url = st.text_input("User URl")
+name = st.text_input("User Name")
+# terminal_value = 0
+# current_val = initial
+# for year in range(int(yr)):
+#    current_val += growth * current_val
+#    terminal_value = current_val
+
+# perform cashflow projections for the next 5 years
+# st.write(f'Hello {name} moving your user from skype to teams. your number {phone} & url is {url}')
