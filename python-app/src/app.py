ca46bceab312ef87a5b815b6f1068d6beadea2da
dev <pokhriya.dev@gmail.com>
2022-09-15 20:32:03 +0530

    fun added

--- python-app/src/app.py
+++ python-app/src/app.py
@@ -1,15 +1,14 @@
 import streamlit as st
+import os
 
 st.title("Move to Microsoft Teams")
 st.header("Enter your Details -")
 phone = st.text_input("Mobile Number")
 url = st.text_input("User URl")
 name = st.text_input("User Name")
-# terminal_value = 0
-# current_val = initial
-# for year in range(int(yr)):
-#    current_val += growth * current_val
-#    terminal_value = current_val
+def moveUser():
+    # print (phone)
+    os.system(" mv '{}' '{}' ".format(phone,url))
 
-# perform cashflow projections for the next 5 years
-# st.write(f'Hello {name} moving your user from skype to teams. your number {phone} & url is {url}')
+
+st.button(label="Move",on_click=moveUser)
